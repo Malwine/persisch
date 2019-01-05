ff4fdaa2d13a268af9d86b3050154cb167492607
Malwine <malwine@mailbox.org>
2019-01-05 18:45:43 +0100

    Remove former prgress and use two css classes

--- src/components/box/index.js
+++ src/components/box/index.js
@@ -4,24 +4,12 @@ import style from './style';
 
 export default class Box extends Component {
 
-  showProgress = (progress) => {
-      let progressBar = []
-
-      {for (let block = 1; block <= progress; block++) {
-          progressBar.push(<div class={style.bar}></div>)
-      }}
-      return progressBar
-  }
-
 render({ link, headline, description, progress, back }) {
   return (
-    <div class={style.box}>
+    <div class={ back ? [style.box, style.gray].join(' ') : style.box}>
       { headline && <h2 class={style.headline}>{ headline }</h2>}
       { description && <p class={style.subline}>{description}</p>}
       { link && <a class={style.button} href={ link }>LEARN</a>}
-
-      { back && (progress !== null && progress !== undefined  && progress !== 0) ? 
-        <div class={ style.progressWrap }>{ this.showProgress(progress) }</div> : <div></div> }
     </div>
     )
   }
