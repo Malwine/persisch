4e242bdcd8c20790b071f158f784066af38b8108
Malwine <malwine@mailbox.org>
2019-01-08 13:34:25 +0100

    Update home page

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,12 +1,24 @@
 import { h } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
+import Box from '../../components/box'
 
 const Home = () => (
 	<div class={style.home}>
-		<h2>Flashcards</h2>
-		<p>Learn anywhere anytime!</p>
-		<p>Check out some of the <Link activeClassName={style.active} href="/sets">sample sets</Link>.</p>
+		<h2>Learn wherever you go!</h2>
+		<div class={style.aboveBox}>
+			<p>Flashcards is a web app that helps you practice your vocabulary wherever you go.
+			Learn on your way to school or work. The app will be available independent of 
+			your internet connection.</p>
+		</div>
+
+		<Box headline={ "four" } description={ "in Persian" } />
+
+		<div class={style.belowBox}>
+			<p>Add the app to your home screen and start learning.</p>
+			<p>Check out some of the <Link class={style.link} href="/sets">sample sets</Link>.</p>
+			<p>This app was built by <Link class={style.link} href="https://twitter.com/malweene">Malwine</Link></p>
+		</div>
 	</div>
 );
 
