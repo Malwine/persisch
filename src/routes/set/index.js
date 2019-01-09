671c79bfd5ad6902b61e8bab19ae4d18d90a7540
Malwine <malwine@mailbox.org>
2019-01-09 15:29:03 +0100

    Refactor progress and reset and fix bug

--- src/routes/set/index.js
+++ src/routes/set/index.js
@@ -2,7 +2,7 @@ import { h, Component } from 'preact';
 import style from './style';
 import { start } from '../../lib/flashcards'
 import { route } from 'preact-router'
-import { resetSet } from '../../lib/flashcards'
+import { reset, getProgressForSet } from '../../lib/flashcards'
 
 export default class Set extends Component {
 
@@ -14,23 +14,32 @@ export default class Set extends Component {
 
 	handleResetClick = () => {
 		let currentSet = this.props.data.sets[this.props.set]
-		currentSet = resetSet(this.props.data.sets[this.props.set])
+		reset(currentSet)
+		this.forceUpdate()
 	}
 
 	render({ data, set: setIndex }) {
+		const set = data.sets[setIndex];
+		const progress = getProgressForSet(set);
+
 		return (
 			<div class={style.spacing}>
-				<h2>{data.sets[setIndex].name}</h2>
-				<p class={style.subline}>{data.sets[setIndex].description}</p>
-				<button 
+				<h2>{set.name}</h2>
+				<p class={style.subline}>{set.description}</p>
+				{ progress }
+				{ progress >= 100 ? <div>Finished</div> : (
+					<button 
 					class={[style.button, style.buttonPrimary].join(' ')} 
 					onClick={ this.handleStartClick }>
 					Start
-				</button>
+					</button> 
+				) 
+				}
+				
         
         <p>Included words:</p>
         <ul class={style.list}>	
-				{ data.sets[setIndex].cards.map((card, index) => {
+				{ set.cards.map((card, index) => {
 					return (
             <li class={ style.cardWrap }>
               <div>{ card.front }</div>
