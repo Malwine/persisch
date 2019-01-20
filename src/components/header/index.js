44ac8a75ba104e57877f79b6e7b0e94b8aeb9589
Malwine <malwine@mailbox.org>
2019-01-20 22:14:36 +0100

    Import the arrow correctly

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,13 @@
 import { h } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
+import arrow from '../../assets/icons/arrow.png'
 
 const Header = ({backButtonLocation}) => (
 	<header class={style.header}>
 		{ backButtonLocation ? (
 				<Link class={style.arrow} href={backButtonLocation}>
-					<img class={style.arrow} src="../../assets/icons/arrow.png"></img>
+					<img class={style.arrow} src={ arrow }></img>
 				</Link> 
 			) : (
 			<div class={style.space}></div>
@@ -14,7 +15,6 @@ const Header = ({backButtonLocation}) => (
 		}
 		<Link class={style.name} href="/"><h1>Flashcards</h1></Link>
 		<Link activeClassName={style.active} href="/sets">Sets</Link>
-
 	</header>
 );
 
