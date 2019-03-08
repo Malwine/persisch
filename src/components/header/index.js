8f53864392e7583c2fb4d82a381cb77269d7e1e6
Malwine <malwine@mailbox.org>
2019-03-08 19:25:35 +0100

    Tab space for arrow

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,6 +2,7 @@ import { h } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
 import arrow from '../../assets/icons/arrow.png'
+import info from '../../assets/icons/info.png'
 
 const Header = ({backButtonLocation}) => (
   <header class={style.header}>
@@ -10,7 +11,9 @@ const Header = ({backButtonLocation}) => (
           <img class={style.icon} src={ arrow }></img>
         </Link>
       ) : (
-      <Link class={style.icon} href="/info">Info</Link>
+      <Link class={style.arrow} href="/info">
+        <img class={style.icon} src={ info }></img>
+      </Link>
       )
     }
     <Link class={style.name} href="/"><h1>Persisch</h1></Link>
