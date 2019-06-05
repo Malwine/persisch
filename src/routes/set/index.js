f4a1f091b279992f740f0a36b3e823e065c6600c
Malwine <malwine@mailbox.org>
2019-06-05 22:16:53 +0200

    Update

--- src/routes/set/index.js
+++ src/routes/set/index.js
@@ -6,53 +6,53 @@ import { reset, getProgressForSet } from '../../lib/flashcards'
 
 export default class Set extends Component {
 
-	handleStartClick = () => {
-		let cardIndex = start(this.props.data, this.props.set)
-		route(`/sets/${this.props.set}/cards/${cardIndex}`)
-	}
+  handleStartClick = () => {
+    let cardIndex = start(this.props.data, this.props.set)
+    route(`/sets/${this.props.set}/cards/${cardIndex}`)
+  }
 
-	handleResetClick = () => {
-		let currentSet = this.props.data.sets[this.props.set]
-		if (confirm("Do you want to reset your progress?")) {
+  handleResetClick = () => {
+    let currentSet = this.props.data.sets[this.props.set]
+    if (confirm("Do you want to reset your progress?")) {
       reset(currentSet)
     }
-		this.forceUpdate()
-	}
+    this.forceUpdate()
+  }
 
-	render({ data, set: setIndex }) {
-		const set = data.sets[setIndex];
-		const progress = getProgressForSet(set);
+  render({ data, set: setIndex }) {
+    const set = data.sets[setIndex];
+    const progress = getProgressForSet(set);
 
-		return (
-			<div class={style.spacing}>
-				<h2>{set.name}</h2>
-				<p class={style.subline}>{set.description}</p>
-				{ progress >= 100 ?	
-					( <div class={style.finished}>✓ Completed</div> ) : 
-					progress > 0 ?
-					( <button class={[style.button, style.buttonPrimary].join(' ')} 
-							onClick={ this.handleStartClick }>Resume</button>) :
-					( <button class={[style.button, style.buttonPrimary].join(' ')} 
-							onClick={ this.handleStartClick }>Start</button> ) 
-				}
+    return (
+      <div class={style.spacing}>
+        <h2>{set.name}</h2>
+        <p class={style.subline}>{set.description}</p>
+        { progress >= 100 ?
+          ( <div class={style.finished}>✓ Completed</div> ) :
+          progress > 0 ?
+          ( <button class={[style.button, style.buttonPrimary].join(' ')}
+              onClick={ this.handleStartClick }>Weiterlernen</button>) :
+          ( <button class={[style.button, style.buttonPrimary].join(' ')}
+              onClick={ this.handleStartClick }>Start</button> )
+        }
         <p>Included words:</p>
-        <ul class={style.list}>	
-				{ set.cards.map((card, index) => {
-					return (
+        <ul class={style.list}>
+        { set.cards.map((card, index) => {
+          return (
             <li class={ style.cardWrap }>
               <div>{ card.front }</div>
               <div class={ style.left }>{ card.back }</div>
             </li>
-					)
-				})}
-				</ul>
+          )
+        })}
+        </ul>
 
-				<button 
-					class={[style.button, style.resetButton].join(' ')} 
-					onClick={ this.handleResetClick }>
-					zurücksetzen
-				</button>
-			</div>
-		);
-	}
+        <button
+          class={[style.button, style.resetButton].join(' ')}
+          onClick={ this.handleResetClick }>
+          zurücksetzen
+        </button>
+      </div>
+    );
+  }
 }
